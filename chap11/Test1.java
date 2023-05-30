3eb24f8d2623f1d710ceddec90dbdf67f9e22ba5
duguohao <127091637+duguohao@users.noreply.github.com>
2023-05-30 17:14:32 +0900

    Add files via upload

--- chap11/Test1.java
+++ chap11/Test1.java
@@ -1,43 +1,33 @@
 package chap11;
 abstract class abs{
 	
-	abstract void abs(int a,int b);
-	
-	
+	public  abstract int add(int a,int b);
+	public  abstract int sub(int a,int b);
+	public  abstract double avg(int[]a);
 }
-class add extends abs {
-	void abs(int a,int b) {
-		System.out.println(a+b);
+class Goodcale extends abs {
+	public int add(int a,int b) {
+		return a+b;
+	} 
+	public int sub(int a,int b) {
+		return a-b;
 	}
-}
-class sub extends abs {
-	void abs(int a,int b) {
-		System.out.println(a-b);
-	}
-}
-class mul extends abs{
-	void abs(int a,int b) {
-		System.out.println(a*b);
-	}
-	
-}
-class div extends abs{
-	void abs(int a,int b) {
-		System.out.println(a/b);
-	}
-	
-}
-public   class Test1 {
-	public static void main(String[]args) {
-		abs s=new add();
-		s.abs(50, 50);
-		s=new sub();
-		s.abs(50, 50);
-		s=new mul();
-		s.abs(50, 50);
-		s=new div();
-		s.abs(50, 50);
-		
+	public double avg(int[]a) {
+		double sum=0;
+		for(int i=0;i<a.length;i++)
+			sum+=a[i];
+		return sum/a.length;
 	}
 
+
 }
+	
+
+public   class Test1 {
+	public static void main(String[]args) {
+		Goodcale g=new Goodcale();
+		g.add(50, 50);
+	
+	}
+}
+
